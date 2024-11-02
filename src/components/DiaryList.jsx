2476b33c8ff1760976b8dd6b79f4ea349d222507
haha0ha <haha0hazzang@gmail.com>
2024-11-02 15:28:25 +0900

    Initial commit

---
+++ src/components/DiaryList.jsx
@@ -0,0 +1,15 @@
+const DiaryList = () =>{
+
+    return(
+        <div className="DiaryList">
+            <div className="menu_bar">
+                <select>
+                    <option></option>
+                </select>
+            </div>
+            <div className="list_wrapper"></div>
+        </div>
+    );
+};
+
+export default DiaryList;
